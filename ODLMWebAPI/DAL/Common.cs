c04e48738d08c56cc286ce55f04a3c41224392cc
Pandurang Vhare <pandurang.vhare@vegainnovations.co.in>
2020-06-18 19:20:24 +0530

    commit changes

--- ODLMWebAPI/DAL/Common.cs
+++ ODLMWebAPI/DAL/Common.cs
@@ -133,7 +133,7 @@ namespace ODLMWebAPI.DAL
                 String sqlQuery = "SELECT CURRENT_TIMESTAMP AS ServerDate";
 
                 ////To get Server Date Time for Azure Server DB
-                //string sqlquery = " declare @dfecha as datetime " +
+                //string sqlQuery = " declare @dfecha as datetime " +
                 //                  " declare @d as datetimeoffset " +
                 //                  " set @dfecha= sysdatetime()   " +
                 //                  " set @d = convert(datetimeoffset, @dfecha) at time zone 'india standard time'" +
