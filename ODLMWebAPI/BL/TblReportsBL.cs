1de9db5ca1a9727955a4435f27d677180c1f4c20
kiran.mehetre <kiran.mehetre@vegainnovations.co.in>
2020-06-24 18:05:52 +0530

    commit changes

--- ODLMWebAPI/BL/TblReportsBL.cs
+++ ODLMWebAPI/BL/TblReportsBL.cs
@@ -256,7 +256,7 @@ namespace ODLMWebAPI.BL
                                         int loadingSlipDTrowNorowNo = loadingSlipExtDT.Rows.Count - 1;
                                         loadingSlipExtDT.Rows[loadingSlipDTrowNorowNo]["idLoadingSlip"] = TblLoadingSlipTO.IdLoadingSlip;
                                         string displayName = tblLoadingSlipExtTO.MaterialDesc + "/" + tblLoadingSlipExtTO.ProdCatDesc + "/" + tblLoadingSlipExtTO.ProdSpecDesc;
-                                        if(displayName=="--")
+                                        if(displayName=="//")
                                             displayName=tblLoadingSlipExtTO.DisplayName;
                                         loadingSlipExtDT.Rows[loadingSlipDTrowNorowNo]["Size"] = displayName;// tblLoadingSlipExtTO.DisplayName;
                                         loadingSlipExtDT.Rows[loadingSlipDTrowNorowNo]["prodItem"] = tblLoadingSlipExtTO.ProdItemDesc;
